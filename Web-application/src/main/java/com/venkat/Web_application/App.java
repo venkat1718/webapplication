dbd561c46d57cf2328569bca013a8a0a6c78fb4a
Venkat Appanaboina <avenkat616@gmail.com>
2019-04-18 12:08:09 +0530

    hi

---
+++ Web-application/src/main/java/com/venkat/Web_application/App.java
@@ -0,0 +1,13 @@
+package com.venkat.Web_application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
